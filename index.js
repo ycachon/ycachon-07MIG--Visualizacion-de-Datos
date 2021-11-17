76194edcf5fd23bf0dadd724fa1589e652172e80
ycachon <ycachon@gmail.com>
2021-11-17 19:13:15 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,39 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var visualization = d3plus.viz()
+  .container("#viz1")
+  .data([{"year": 1991, "name":"alpha", "value": 15},
+{   "year": 1992, "name":"alpha", "value": 20},])
+  .type("bar")
+  .id("name")
+  .x("year")
+  .y("value")
+  .draw()
+
+var visualization_bigotes = d3plus.viz()
+  .container("#viz3")
+  .data([
+    {"year": 1991, "name":"alpha", "value": 15},
+    {"year": 1992, "name":"alpha", "value": 34},
+    {"year": 1991, "name":"alpha2", "value": 17},
+    {"year": 1992, "name":"alpha2", "value": 65},
+    {"year": 1991, "name":"beta", "value": 10},
+    {"year": 1992, "name":"beta", "value": 10},
+    {"year": 1991, "name":"beta2", "value": 40},
+    {"year": 1992, "name":"beta2", "value": 38},
+    {"year": 1991, "name":"gamma", "value": 5},
+    {"year": 1992, "name":"gamma", "value": 10},
+    {"year": 1991, "name":"gamma2", "value": 20},
+    {"year": 1992, "name":"gamma2", "value": 34},
+    {"year": 1991, "name":"delta", "value": 50},
+    {"year": 1992, "name":"delta", "value": 43},
+    {"year": 1991, "name":"delta2", "value": 17},
+    {"year": 1992, "name":"delta2", "value": 35}
+  ])
+  .type("box")
+  .id("name")
+  .x("year")
+  .y("value")
+  .draw()
